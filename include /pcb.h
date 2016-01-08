e61994ae3d6f3d00bd2f83331fd4d5cffb02e7cb
simone.preite <simone-preite_89@hotmail.it>
2016-01-08 20:29:09 +0100

    up

--- include /pcb.h
+++ include /pcb.h
@@ -1,1 +1,25 @@
+#ifndef PCB_H
+#define PCB_H
+
+/* prototipi funzioni liste */
+
+/* Lista */
+
+EXTERN void freePcb(pcb_t *p);
+EXTERN pcb_t *allocPcb(void);
+EXTERN void initPcbs(void);
+
+EXTERN void insertProcQ(struct clist *q, pcb_t *p);
+EXTERN pcb_t *removeProcQ(struct clist *q);
+EXTERN pcb_t *outProcQ(struct clist *q, pcb_t *p);
+EXTERN pcb_t *headProcQ(struct clist *q);
+
+/* Alberi */
+
+EXTERN int emptyChild(pcb_t *p);
+EXTERN void insertChild(pcb_t *parent, pcb_t *p);
+EXTERN pcb_t *removeChild(pcb_t *p);
+EXTERN pcb_t *outChild(pcb_t *p);
+
+
 
