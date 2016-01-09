ece342aa89bd0ff6df0ab15f982ea7cbc8925d83
simone.preite <simone-preite_89@hotmail.it>
2016-01-09 14:53:05 +0100

    up

--- include/pcb.h
+++ include/pcb.h
@@ -5,22 +5,6 @@
 
 /* Lista */
 
-/*EXTERN void freePcb(struct pcb_t *p);
-EXTERN struct pcb_t *allocPcb(void);
-EXTERN void initPcbs(void);
-
-EXTERN void insertProcQ(struct clist *q, struct pcb_t *p);
-EXTERN struct pcb_t *removeProcQ(struct clist *q);
-EXTERN struct pcb_t *outProcQ(struct clist *q, struct pcb_t *p);
-EXTERN struct pcb_t *headProcQ(struct clist *q);
-
-/* Alberi */
-
-/*EXTERN int emptyChild(struct pcb_t *p);
-EXTERN void insertChild(struct pcb_t *parent, struct pcb_t *p);
-EXTERN struct pcb_t *removeChild(struct pcb_t *p);
-EXTERN struct pcb_t *outChild(struct pcb_t *p);*/
-
 EXTERN void freePcb(pcb_t *p);
 EXTERN pcb_t *allocPcb(void);
 EXTERN void initPcbs(void);
@@ -30,7 +14,7 @@ EXTERN pcb_t *removeProcQ(pcb_t *q);
 EXTERN pcb_t *outProcQ(pcb_t *q, pcb_t *p);
 EXTERN pcb_t *headProcQ(struct clist *q);
 
-/* Funzioni per alberi */
+/* Alberi */
 
 EXTERN int emptyChild(pcb_t *p);
 EXTERN void insertChild(pcb_t *parent, pcb_t *p);
