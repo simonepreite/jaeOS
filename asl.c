83b0f64affa716185706e2c73336b8ff7f7333e7
simone.preite <simone-preite_89@hotmail.it>
2016-01-12 00:40:31 +0100

    up

--- asl.c
+++ asl.c
@@ -22,21 +22,50 @@ void initASL(){
 	}
 }
 
+/* 
+1° caso: 
+	cerca il semaforo nella lista aslh e nel momento in cui lo trova
+	inserisce il processo puntato da p nella lista dei processi
+	bloccati in quel semaforo e setta il puntatore al semaforo
+	a semAdd
+2° caso:
+	se il semaforo non c'è e la lista semdFree è vuota ritorna TRUE
+3° caso:
+	se il semaforo non c'è lo alloca prendendono dalla lista semdfree
 
+	ovviamente non funziona ancora
+*/
 
-int insertBlocked(int *semAdd, struct pcb_t *p);
-	/* 
-	1° caso: 
-		cerca il semaforo nella lista aslh e nel momento in cui lo trova
-		inserisce il processo puntato da p nella lista dei processi
-		bloccati in quel semaforo e setta il puntatore al semaforo
-		a semAdd
-	2° caso:
-		se il semaforo non c'è e la lista semdFree è vuota ritorna TRUE
-	3° caso:
-		se il semaforo non c'è lo alloca prendendono dalla lista semdfree
-
-
+int insertBlocked(int *semAdd, struct pcb_t *p){
+	struct semd_t *scan, *addr;
+	void *tmp;
+	int trovato = 0;
+	clist_foreach(scan, &aslh, s_link, tmp){
+		if(semAdd == scan->s_semAdd){
+			clist_enqueue(p, &aslh, p_list);
+			addr = container_of(semAdd, typeof(*addr), s_proc);
+			p->p_cursem = addr; 
+			trovato = 1;
+			return FALSE;
+		}
+	}
+	if(semdFree.next != NULL){
+		clist_foreach(scan, &aslh, s_link, tmp) {
+			if (semAdd < scan->s_semAdd) {
+				dequeue(semdFree);
+				clist_foreach_add(semAdd, scan->s_semAdd, &aslh, s_link, tmp);
+				}
+		}
+			if (clist_foreach_all(scan, &aslh, s_link,tmp)) 
+				clist_enqueue(semAdd, &myclist, s_link);
+		scan->s_semAdd = semAdd;
+		clist_enqueue(p, &aslh, s_proc);
+		addr = container_of((semAdd), typeof(*addr), s_proc);
+		p->p_cursem = addr; 
+		return FALSE;
+	}
+	else return TRUE;
+}
 
 struct pcb_t *removeBlocked(int *semAdd);
 
