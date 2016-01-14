c1de844f63adf2c31739454d6e1b74bd3b781fde
simone.preite <simone-preite_89@hotmail.it>
2016-01-14 13:53:51 +0100

    up

--- asl.c
+++ asl.c
@@ -14,7 +14,7 @@ HIDDEN struct clist aslh, semdFree;
 void initASL(){
 	static struct semd_t semdTable[MAXPROC];   
 	int i;
-
+	
 	aslh.next = NULL;
 	semdFree.next = &semdTable[MAXPROC-1].s_link;
 	semdTable[MAXPROC-1].s_link.next = &semdTable[0].s_link;
@@ -61,46 +61,41 @@ int insertBlocked(int *semAdd, struct pcb_t *p){
 			return FALSE;
 		} 
 		else { //semaforo non trovato
-	
 			if (semdFree.next != NULL) { //semdFree non vuota
-		
 				new_sem = container_of(semdFree.next->next, typeof(*new_sem), s_link);
 				p->p_cursem = new_sem;
 				new_sem->s_semAdd = semAdd;
 				new_sem->s_proc.next = NULL;
 				clist_dequeue(&semdFree);
-		
 				insertProcQ(&new_sem->s_proc, p);
-		
 				if(aslh.next == NULL) {
 					clist_enqueue(new_sem, &aslh, s_link);
-			
 					return FALSE;
 				}
 					clist_foreach(scan, &aslh, s_link, tmp){
-				
 						if(new_sem->s_semAdd < scan->s_semAdd){
 							clist_foreach_add(new_sem, scan, &aslh, s_link, tmp);
-					
 							break;
 						}
 					}
 						if (clist_foreach_all(scan, &aslh, s_link, tmp)) {
 							clist_enqueue(new_sem, &aslh, s_link); 
-					
 						}
-		
 				return FALSE;
 				}
 			else { // caso lista semdFree vuota
-		
 				return TRUE; 
 				}
 			}	
 	}
 }
 
+/*
 
+rimuove il primo processo dalla coda dei processi bloccati nel 
+semaforo puntato da semAdd
+
+*/
 
 struct pcb_t *removeBlocked(int *semAdd){
 	struct pcb_t *p;
@@ -126,7 +121,12 @@ struct pcb_t *removeBlocked(int *semAdd){
 	}
 }
 
+/*
 
+rimuove dalla coda dei processi bloccati nel semaforo puntato
+da p->p_cursem il processo p
+
+*/
 
 struct pcb_t *outBlocked(struct pcb_t *p){
 	struct semd_t *scan;
@@ -144,7 +144,11 @@ struct pcb_t *outBlocked(struct pcb_t *p){
 	if(!trovato) return NULL;
 }
 
+/*
 
+ritorna il puntatore alla testa dei processi bloccati in semAdd
+
+*/
 
 struct pcb_t *headBlocked(int *semAdd){
 	struct pcb_t *p;
