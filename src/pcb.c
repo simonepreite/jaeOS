d9aff9ebd0ca33bcb6ddd5dfbdc4b56f669b633d
pierluigi <pierluigi@localhost.localdomain>
2016-02-01 11:03:56 +0100

    modificato il tipo di pcbFree che non doveva essere un puntatore

--- src/pcb.c
+++ src/pcb.c
@@ -4,7 +4,7 @@
 
 /* Lista dei pcb inutilizzati */
 
-HIDDEN struct clist *pcbFree = NULL;
+HIDDEN struct clist pcbFree = CLIST_INIT;
 
 /***************************************************************
 *           ALLOCATION AND DEALLOCATION PROCKBLKS'S            *
@@ -13,7 +13,7 @@ HIDDEN struct clist *pcbFree = NULL;
 //inserisce il pcb passato come parametro in coda alla lista
 
 void freePcb(struct pcb_t *p){
-	struct clist *t = pcbFree;
+	struct clist *t = &pcbFree;
 	
 	clist_enqueue(p, t, p_list);
 }
@@ -29,9 +29,9 @@ void freePcb(struct pcb_t *p){
 struct pcb_t *allocPcb(){
 	struct pcb_t *pcb = NULL;
 
-		if(pcbFree->next != NULL){
-			pcb = container_of((pcbFree)->next->next, typeof(*pcb), p_list);
-			clist_dequeue(pcbFree);
+		if(pcbFree.next != NULL){
+			pcb = container_of(pcbFree.next->next, typeof(*pcb), p_list);
+			clist_dequeue(&pcbFree);
 			pcb->p_parent = NULL;
 			pcb->p_cursem = NULL;
 			{
@@ -53,7 +53,7 @@ void initPcbs(){
 	static struct pcb_t pcb_static[MAXPROC];
     int i;
 
-	pcbFree->next = &pcb_static[MAXPROC-1].p_list;
+	pcbFree.next = &pcb_static[MAXPROC-1].p_list;
 	pcb_static[MAXPROC-1].p_list.next = &pcb_static[0].p_list;
 	for(i = 0; i < (MAXPROC-1); i++){
 		pcb_static[i].p_list.next = &pcb_static[i+1].p_list;
