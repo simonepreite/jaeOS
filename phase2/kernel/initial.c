623e435ebe13f6596562ef75d290d1326cc2ee24
simone preite <simone-preite_89@hotmail.it>
2016-06-22 21:28:56 +0200

    added init area

--- phase2/kernel/initial.c
+++ phase2/kernel/initial.c
@@ -6,7 +6,20 @@ UI softBlockCounter;
 pcb_t *readyQueue;
 pcb_t *curProc;
 
-
+void initArea(memaddr area, memaddr handler){
+    state_t *newArea = (state_t*) area;
+    /* Memorizza il contenuto attuale del processore in newArea */
+    STST(newArea);
+    /* Setta pc alla funzione che gestirà l'eccezione */
+    newArea->pc = handler;
+    /* Setta sp a RAMTOP */
+    newArea->sp = RAMTOP;
+    /* Setta il registro di Stato per mascherare tutti gli interrupt e si mette in kernel-mode. */
+    newArea->cpsr=STATUS_ALL_INT_DISABLE((newArea->cpsr) | STATUS_SYS_MODE);
+    /* Disabilita la memoria virtuale */
+        /* setCONTROL((getCONTROL()) & ~(DISABLE_VM));*/
+    newArea->CP15_Control = (newArea->CP15_Control) & ~(ENABLE_VM);
+}
 
 
 int main(){
@@ -19,7 +32,12 @@ int main(){
 
     curProc = NULL;
 
-	initPcbs();
+		initArea(INT_NEWAREA, (memaddr)intHandler);
+		initArea(TLB_NEWAREA, (memaddr)tlbHandler);
+		initArea(PGMTRAP_NEWAREA, (memaddr)pgmHandler);
+		initArea(SYSBK_NEWAREA, (memaddr)sysBpHandler);
+
+		initPcbs();
 
     initASL();
 
