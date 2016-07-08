800809bd5fe6e03371f5c7be18d9ee9302e33602
simone preite <simone-preite_89@hotmail.it>
2016-07-08 22:52:44 +0200

    up

--- phase2/kernel/exceptions.c
+++ phase2/kernel/exceptions.c
@@ -1,8 +1,8 @@
 #include <exceptions.h>
 
-state_t *tlb_old = (state_t*) TLB_OLDAREA;
-state_t *pgmtrap_old = (state_t*) PGMTRAP_OLDAREA;
-state_t *sysbp_old = (state_t*) SYSBK_OLDAREA;
+state_t *tlb_old = (state_t*)TLB_OLDAREA;
+state_t *pgmtrap_old = (state_t*)PGMTRAP_OLDAREA;
+state_t *sysbp_old = (state_t*)SYSBK_OLDAREA;
 
 
 void pgmHandler(){
